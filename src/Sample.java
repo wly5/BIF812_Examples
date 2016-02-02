fad2ac527521d2ab0b4e23e7c22315e178426842
gares <gares@gares-virtual-machine>
2016-02-02 14:36:14 -0500

    initial commit with Sample.java

---
+++ src/Sample.java
@@ -0,0 +1,29 @@
+/**
+ * This class stores data about a sample
+ * @author gares
+ *
+ */
+public class Sample {
+	//an id field to identify the sample
+	long id;
+	private String description;
+
+	public Sample(){
+		id = -1;
+		description = "NOT INITIALIZED";
+	}
+	
+	public Sample(long newId, String desc){
+		id = newId;
+		description = desc;
+	}
+	
+	/**
+	 * this method returns a string that contains this object's information
+	 * in a human readable string
+	 */
+	public String toString(){
+		return "Contents: ID: " + id + "description: "+ description; 
+	}
+	
+}
