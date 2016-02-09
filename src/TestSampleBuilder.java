34db43aa5eb23462903c4fce30fad07cd4c62a3e
gares <gares@gares-virtual-machine>
2016-02-09 13:52:49 -0500

    wrote a test to test the 10MB file

--- src/TestSampleBuilder.java
+++ src/TestSampleBuilder.java
@@ -3,7 +3,7 @@ public class TestSampleBuilder {
 
 	public static void main(String[] args) {
 		Sample mySample = new SampleBuilder();
-		long nanoseconds = mySample.testConcatenate(10000);
+		long nanoseconds = mySample.testConcatenate(100);
 		double seconds = nanoseconds / (double)1000000000; 
 		System.out.println("concatenating using StringBuilder took " + seconds + " seconds");
 
